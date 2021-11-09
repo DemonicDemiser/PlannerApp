a71601feddd8bcac1f3591c5273e117ff9b926bd
DemonicDemiser <waabaki921642@gmail.com>
2021-11-09 23:55:53 +0600

    first commit

---
+++ src/main/java/com/example/planner/ApplicationController.java
@@ -0,0 +1,19 @@
+package com.example.planner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ApplicationController {
+
+    @GetMapping("/index")
+    public String home(){
+        return "index";
+    }
+
+
+    @GetMapping("/users-profile")
+    public String components_alerts(){
+        return "users-profile";
+    }
+}
